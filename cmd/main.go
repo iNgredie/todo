5d0278b46f407c790d7d112cda453a219dbc13c3
iNgredie <sandslash@bk.ru>
2023-08-28 01:02:31 +0400

    add other handlers

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	handler := new(handlers.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run(":8000", handler.InitRoutes()); err != nil {
+	if err := srv.Run("8000", handler.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
